d47caffb6cf0a172c04acc795842c4d4f541faa2
Muhammad Annas Baig <83984612+Annas03@users.noreply.github.com>
2022-10-09 12:36:41 +0500

    Update main.cpp

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "node.h"
 using namespace std;
 
+node* insertNode(node* root_Node, int val);
+void deleteNode();
+
 int main(){
+    int arr[] = {4,2,3,6,5,7,1};
+    node* root_Node;
+    for(int i = 0; i<7; i++){
+        root_Node = insertNode(root_Node, arr[i]);
+    }
+    cout<<root_Node->right_child->val<<endl;
     return 0;
 }
+
+node* insertNode(node* root_Node, int val){
+    if(root_Node == NULL){
+        root_Node = new node(val);
+        return root_Node;
+    }
+    else if(root_Node->val > val){
+        root_Node->left_child = insertNode(root_Node->left_child, val); 
+    }
+    else if(root_Node->val < val){
+        root_Node->right_child = insertNode(root_Node->right_child, val);
+    }
+    return root_Node;
+}
+
+// void deleteNode(){
+
+// }
