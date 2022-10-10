e1701e0b189af8d92393848d90c81ad412712cd4
Muhammad Annas Baig <83984612+Annas03@users.noreply.github.com>
2022-10-10 21:37:31 +0500

    Update main.cpp

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 node* insertNode(node* root_Node, int val);
 node* SearchNode(node* root_Node, int val);
-void deleteNode(node* root_Node, int val);
+node* deleteNode(node* root_Node, int val);
 
 int main(){
     int arr[] = {4,2,3,6,5,7,1};
@@ -12,7 +12,8 @@ int main(){
     for(int i = 0; i<7; i++){
         root_Node = insertNode(root_Node, arr[i]);
     }
-    cout<<SearchNode(root_Node, 3)->val<<endl;
+    root_Node = deleteNode(root_Node,1);
+    cout<<root_Node->left_child->left_child<<endl;
     return 0;
 }
 
@@ -32,7 +33,7 @@ node* insertNode(node* root_Node, int val){
 
 node* SearchNode(node* root_Node,int val){
     while(root_Node != NULL){
-        if(root_Node->val == val){
+        if(root_Node->left_child->val == val || root_Node->right_child->val == val){
             return root_Node;
         }
         else if(root_Node->val > val){
@@ -45,7 +46,25 @@ node* SearchNode(node* root_Node,int val){
     return NULL;
 }
 
-void deleteNode(node* root_Node, int val){
-    SearchNode(root_Node, val);
+node* deleteNode(node* root_Node, int val){
+    node* Parent_Node = SearchNode(root_Node, val);
+    node* child_Node;
+    if(Parent_Node == 0){
+        cout<<"Key not found"<<endl;
+        return NULL;
+    }
+    if(Parent_Node->left_child->val == val){
+        if(Parent_Node->left_child->left_child == NULL && Parent_Node->left_child->right_child == NULL){
+            delete Parent_Node->left_child;
+            Parent_Node->left_child = NULL;
+        }
+    }
+    else if(Parent_Node->right_child->val == val){
+        if(Parent_Node->right_child->left_child == NULL && Parent_Node->right_child->right_child == NULL){
+            delete Parent_Node->right_child;
+            Parent_Node->right_child = NULL;
+        }
+    }
 
+    return root_Node;
 }
