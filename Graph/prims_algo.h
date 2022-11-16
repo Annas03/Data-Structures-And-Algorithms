b909b314a1bda185bb7573a88d62fd1b2845625d
Muhammad Annas Baig <83984612+Annas03@users.noreply.github.com>
2022-11-16 14:35:32 +0500

    Update prims_algo.h

--- Graph/prims_algo.h
+++ Graph/prims_algo.h
@@ -16,14 +16,15 @@ void prims_MST(graph& g){
             InitializingEdges(rem_edges,curr_Vertex, g.e*2, g.arr);
             Modifyrem_edges(rem_edges,edges);
             min_edge(g.arr, curr_Vertex, rem_edges, ver,edges);
-            // edges.erase(edges.begin() + min_edge(g.arr, g.v, curr_Vertex, edges, ver), edges.begin() + min_edge(g.arr, g.v, curr_Vertex, edges, ver) + 1);
         } 
+    }
+    // Setting values of all coordinates present in rem_edges to 0.
+    for(int i=0; i<rem_edges.size()/2; i+=2){
+        g.arr[rem_edges[i]][rem_edges[i+1]] = 0;
     }
 }
 
-int min_edge(int** arr,int& curr_Vertex, vector<int>& rem_edges, vector<int>& ver, vector<int>& edges){
-    // find minimum edges coordinates
-    // Update curr_Vertex
+void min_edge(int** arr,int& curr_Vertex, vector<int>& rem_edges, vector<int>& ver, vector<int>& edges){
     int r=0;
     int temp;
     int c=r+1;
